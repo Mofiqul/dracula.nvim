2248691db0e604cb61908cea3b09a7a7441d836c
mofiqul <mofi0islam@gmail.com>
2021-11-03 20:06:02 +0530

    Code refator

--- colors/dracula.lua
+++ colors/dracula.lua
@@ -1,1 +1,13 @@
-require("dracula")
+local dracula = require("dracula")
+
+vim.cmd("hi clear")
+
+if vim.fn.exists("syntax_on") then
+	vim.cmd("syntax reset")
+end
+
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "dracula"
+
+dracula.apply();
