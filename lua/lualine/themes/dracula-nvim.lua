153a6e70e00afc7cd37c54d3823d758af073e503
Frede Braendstrup <frederikbraendstrup@gmail.com>
2021-11-18 00:34:44 +0100

    sorted sections in lualine theme

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -3,8 +3,8 @@ local dracula = {}
 local colors = require('dracula').colors()
 
 dracula.normal = {
-  b = {fg = colors.purple, bg = colors.black},
   a = {fg = colors.black, bg = colors.purple, gui = 'bold'},
+  b = {fg = colors.purple, bg = colors.black},
   c = {fg = colors.white, bg = colors.black}
 }
 
@@ -14,13 +14,13 @@ dracula.visual = {
 }
 
 dracula.inactive = {
+  a = {fg = colors.white, bg = colors.gray, gui = 'bold'},
   b = {fg = colors.black, bg = colors.white},
-  a = {fg = colors.white, bg = colors.gray, gui = 'bold'}
 }
 
 dracula.replace = {
-  b = {fg = colors.yellow, bg = colors.black},
   a = {fg = colors.black, bg = colors.yellow, gui = 'bold'},
+  b = {fg = colors.yellow, bg = colors.black},
   c = {fg = colors.white, bg = colors.black}
 }
 
