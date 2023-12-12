38a228a8d113c6ebef6a6bf2590c18a15cba7172
pynappo <43484729+pynappo@users.noreply.github.com>
2023-12-11 19:00:43 -0800

    revert change to vim.version as well

--- lua/dracula/init.lua
+++ lua/dracula/init.lua
@@ -121,8 +121,8 @@ end
 ---load dracula colorscheme
 ---@param theme string?
 local function load(theme)
-   if vim.version().minor < 7 then
-      vim.api.nvim_err_writeln("dracula.nvim: you must use neovim 0.7 or higher")
+   if vim.fn.has('nvim-0.7') ~= 1 then
+      vim.notify("dracula.nvim: you must use neovim 0.7 or higher")
       return
    end
 
