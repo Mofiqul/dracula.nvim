52335779dfbd72efd8ac32cc7134301606dbbe55
Hampus <hampus@hauffman.se>
2024-10-24 11:58:41 +0200

    updated types

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -6,34 +6,34 @@ local bg_dark = colors.menu or colors.black
 local bg = colors.bg or colors.black
 
 local normal = {
-   a = { fg = colors.black, bg = colors.purple, gui = 'bold' },
+   a = { fg = colors.black, bg = colors.purple, gui = "bold" },
    b = { fg = colors.purple, bg = bg_dark },
    c = { fg = colors.white, bg = bg },
 }
 
 local command = {
-   a = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
+   a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
    b = { fg = colors.cyan, bg = bg_dark },
 }
 
 local visual = {
-   a = { fg = colors.black, bg = colors.pink, gui = 'bold' },
+   a = { fg = colors.black, bg = colors.pink, gui = "bold" },
    b = { fg = colors.pink, bg = bg_dark },
 }
 
 local inactive = {
-   a = { fg = colors.white, bg = colors.visual, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.visual, gui = "bold" },
    b = { fg = colors.black, bg = colors.white },
 }
 
 local replace = {
-   a = { fg = colors.black, bg = colors.yellow, gui = 'bold' },
+   a = { fg = colors.black, bg = colors.yellow, gui = "bold" },
    b = { fg = colors.yellow, bg = bg_dark },
    c = { fg = colors.white, bg = bg },
 }
 
 local insert = {
-   a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+   a = { fg = colors.black, bg = colors.green, gui = "bold" },
    b = { fg = colors.green, bg = bg_dark },
    c = { fg = colors.white, bg = bg },
 }
