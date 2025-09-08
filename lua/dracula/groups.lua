4fc687b57c5a746e9312ca165b23c3425d2979f0
Vadim Shvetsov <vadim@shvetsov.io>
2025-09-08 07:41:25 +0200

    feat: add minimal snacks.nvim picker and dashboard support (#145)

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -614,6 +614,32 @@ local function setup(configs)
       NotifyWarnIcon = { fg = colors.orange },
       NotifyWarnTitle = { fg = colors.orange },
       NotifyWarnBorder = { fg = "#785637" },
+
+      -- SnacksDashboard
+      SnacksDashboardHeader = { fg = colors.purple },
+      SnacksDashboardKey = { fg = colors.orange },
+      SnacksDashboardDesc = { fg = colors.cyan },
+      SnacksDashboardIcon = { fg = colors.cyan },
+      SnacksDashboardFooter = { fg = colors.purple, italic = true },
+
+      -- SnacksPicker
+      SnacksBackdrop = { link = "FloatShadow" },
+      SnacksPickerBorder = { fg = colors.comment },
+      SnacksPickerDir = { fg = colors.fg },
+      SnacksPickerDirectory = { fg = colors.fg },
+      SnacksPickerFile = { fg = colors.fg },
+      SnacksPickerGitStatusIgnored = { fg = colors.comment },
+      SnacksPickerGitStatusModified = { fg = colors.yellow },
+      SnacksPickerGitStatusRenamed = { fg = colors.yellow },
+      SnacksPickerGitStatusStaged = { fg = colors.bright_green },
+      SnacksPickerGitStatusUnmerged = { fg = colors.orange },
+      SnacksPickerGitStatusUntracked = { fg = colors.green },
+      SnacksPickerInput = { link = "NormalFloat" },
+      SnacksPickerInputBorder = { link = "SnacksPickerBorder" },
+      SnacksPickerMatch = { fg = colors.green, italic = true },
+      SnacksPickerPathHidden = { fg = colors.comment },
+      SnacksPickerPrompt = { fg = colors.purple },
+      SnacksPickerTitle = { fg = colors.cyan, bold = true },
    }
 end
 
